603e9b42de202bf19133087ff002241033a32eac
Joshua McMiller <jjmcmi113r@gmail.com>
2022-08-31 16:06:39 -0500

    updated commit

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,9 @@
 
 #!/bin/bash
-cd /home/ec2-user/server/src
+cd /home/ec2-user/server/
+npm install
 npm start
-pm2 start npm --name "reactapp" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+# pm2 start npm --name "reactapp" -- start
+# pm2 startup
+# pm2 save
+# pm2 restart all
